db485c0c83a7fc0fd92ecc91de277cfc09957111
Sri Balaji Gopinath <balaji@babajob.com>
2016-12-20 10:09:36 +0530

    Added logic to check if messages have been stopped

--- plivo.sms/repository/sms_repository.go
+++ plivo.sms/repository/sms_repository.go
@@ -1,18 +1,31 @@
 package repository
 
 import (
+	"fmt"
+	"plivo/plivo.persistence/redis"
 	"plivo/plivo.sms/model"
+	"time"
 )
 
 type SMSRepository struct {
 }
 
 func (smsRepository SMSRepository) SetStop(sms model.SMS) error {
-	return nil
+	key := fmt.Sprintf("from:%s;to:%s", sms.To, sms.From)
+
+	return redis.Set(key, "stop", time.Hour*4)
 }
 
 func (smsRepository SMSRepository) IsStopped(sms model.SMS) (bool, error) {
-	return true, nil
+	key := fmt.Sprintf("from:%s;to:%s", sms.From, sms.To)
+
+	result, _ := redis.Get(key)
+
+	if result == "stop" {
+		return true, nil
+	}
+
+	return false, nil
 }
 
 func (smsRepository SMSRepository) GetSentSMSCount(sms model.SMS) (int, error) {
